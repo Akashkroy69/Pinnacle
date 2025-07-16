d4f53d681eb285ed799a885dc3b68c91a560d0fe
akashkroy <akashkroy@hotmail.com>
2025-07-16 15:35:45 +0530

    react-icons installation

---
+++ pinnacle/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Navbar.module.css';
+import { IoSearchOutline } from "react-icons/io5";
+import { LuSettings2 } from "react-icons/lu";
+import { TbArrowsSort } from "react-icons/tb";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import { LuShoppingCart } from "react-icons/lu";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
