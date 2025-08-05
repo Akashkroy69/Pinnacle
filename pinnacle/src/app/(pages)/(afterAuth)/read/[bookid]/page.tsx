6e99aae2c87529569335e10697b55ddb7a8e028b
akashkroy <akashkroy@hotmail.com>
2025-08-05 21:26:32 +0530

    pdf reader module is completed

--- pinnacle/src/app/(pages)/(afterAuth)/read/[bookid]/page.tsx
+++ pinnacle/src/app/(pages)/(afterAuth)/read/[bookid]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import Navbar from "@/components/Navbar";
+import styles from "./Read.module.css";
+import { useParams, useRouter } from 'next/navigation';
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+    return (
+        <div className={styles.main}>
+            <Navbar />
+            <div className={styles.container}>
+                <div className={styles.viewer}>
+                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                        <Viewer
+                            fileUrl="https://pdfobject.com/pdf/sample.pdf"
+                            plugins={[defaultLayoutPluginInstance]}
+                            theme="dark"
+                        />
+                    </Worker>
+                </div>
+
+            </div>
+        </div>
+    );
+
 }
 
 export default page
